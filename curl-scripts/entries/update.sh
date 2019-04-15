355585307b65ab1e1e08a37efa88b294d3c0aa8d
vickyrockingster <vrockingster@gmail.com>
2019-04-15 15:38:58 -0400

    Can successfully CRUD entry resource via curl-scripts
    -When signed in, I can create, index, show, update, and delete a
    row/rows on the entries table that belong to me

---
+++ curl-scripts/entries/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl "http://localhost:4741/entries/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "entry": {
+      "title": "'"${TITLE}"'",
+      "text": "'"${TEXT}"'"
+    }
+  }'
+
+echo
