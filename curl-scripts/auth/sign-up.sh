481aefb09341ff013762db253bfa56c0fbb3bef6
vickyrockingster <vrockingster@gmail.com>
2019-04-15 12:00:38 -0400

    Can successfully CRUD user table via curl-scripts
    -Can sign up, sign in, sign out, and change password as a user

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -6,9 +6,11 @@ curl "http://localhost:4741/sign-up" \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
+      "first_name": "'"${FIRST}"'",
+      "last_name": "'"${LAST}"'",
       "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'",
-      "password_confirmation": "'"${PASSWORD}"'"
+      "password": "'"${PW}"'",
+      "password_confirmation": "'"${PW}"'"
     }
   }'
 
