9f2819f56080a289493af7cadb7aa44272fc14a5
vickyrockingster <vrockingster@gmail.com>
2019-04-15 14:42:47 -0400

    Generate Scaffolded new resource: entry
    -Set up a one-to-many relationship between users and entries with
    has_many and belongs_to in their respective models
    -Configured routes to leave out new and edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_151738) do
+ActiveRecord::Schema.define(version: 2019_04_15_175824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entries", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text "text", null: false
@@ -35,5 +44,6 @@ ActiveRecord::Schema.define(version: 2019_04_15_151738) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "entries", "users"
   add_foreign_key "examples", "users"
 end
