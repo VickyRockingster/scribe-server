eaf582676ba1acb408c6c07ce9043f120343dc00
vickyrockingster <vrockingster@gmail.com>
2019-04-15 11:48:46 -0400

    Configured user table
    -Added first name and last name columns to the user table
    -Added the first name and last name fields to send back with the
    serializer
    -Added the first name and last name fields to the controller
    params permitted upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ProtectedController
 
   def user_creds
     params.require(:credentials)
-          .permit(:email, :password, :password_confirmation)
+          .permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
   def pw_creds
