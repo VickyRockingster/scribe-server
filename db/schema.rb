9a90d572b9eceabe4ddc2f1e8bc9bca123aba768
vickyrockingster <vrockingster@gmail.com>
2019-04-16 10:06:03 -0400

    Can now successfully sign up from the client
    Could not sign up while first_name column in user table was null: false,
    so removed null: false from that column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_192429) do
+ActiveRecord::Schema.define(version: 2019_04_16_140130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2019_04_15_192429) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name", null: false
     t.string "last_name"
+    t.string "first_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
   end
